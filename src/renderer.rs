0453f5435054d88d1cc16e5d0def3eced9b7cc75
Kaid <kaid@kaid.me>
2024-04-29 00:10:22 +0800

    Prepare to update winit to 0.30.0.

---
+++ src/renderer.rs
@@ -0,0 +1,198 @@
+use winit::dpi::PhysicalPosition;
+use winit::dpi::PhysicalSize;
+use winit::event::WindowEvent;
+use winit::window::Window;
+use wgpu::util::DeviceExt;
+
+use crate::vertex;
+
+pub(crate) struct RendererState<'a> {
+    pub(crate) num_vertices: u32,
+    pub(crate) queue: wgpu::Queue,
+    pub(crate) color: wgpu::Color,
+    pub(crate) device: wgpu::Device,
+    pub(crate) surface: wgpu::Surface<'a>,
+    pub(crate) vertex_buffer: wgpu::Buffer,
+    pub(crate) config: wgpu::SurfaceConfiguration,
+    pub(crate) size: winit::dpi::PhysicalSize<u32>,
+    pub(crate) render_pipeline: wgpu::RenderPipeline,
+    pub(crate) window: &'a Window,
+}
+
+impl<'a> RendererState<'a> {
+    pub fn new(window: &'a Window) -> Self {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window).unwrap();
+
+        let ((device, queue), adapter) = smol::block_on(async {
+            let adapter = instance.request_adapter(&wgpu::RequestAdapterOptionsBase {
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface),
+                power_preference: wgpu::PowerPreference::default(),
+            }).await.unwrap();
+
+            (adapter.request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    required_limits: wgpu::Limits::default(),
+                    required_features: wgpu::Features::empty(),
+                },
+                None,
+            ).await.unwrap(), adapter)
+        });
+
+        let surface_caps = surface.get_capabilities(&adapter);
+
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .copied()
+            .filter(|f| f.is_srgb())
+            .next()
+            .unwrap_or(surface_caps.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+            alpha_mode: surface_caps.alpha_modes[0],
+            present_mode: surface_caps.present_modes[0],
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        };
+
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
+
+        let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("Render Pipeline Layout"),
+            bind_group_layouts: &[],
+            push_constant_ranges:&[],
+        });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[vertex::Vertex::desc()],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
+    
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            usage: wgpu::BufferUsages::VERTEX,
+            contents: bytemuck::cast_slice(vertex::VERTICES),
+        });
+
+        surface.configure(&device, &config);
+
+        let num_vertices = vertex::VERTICES.len() as u32;
+
+        Self { size, queue, device, config, surface, window, color: wgpu::Color { r: 0.1, g: 0.2, b: 0.3, a: 1.0 }, render_pipeline, vertex_buffer, num_vertices }
+    }
+
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    pub(crate) fn input(&mut self, event: &WindowEvent) -> bool {
+        match event {
+            WindowEvent::CursorMoved { position, .. } => {
+                let PhysicalPosition { x, y } = position;
+                let PhysicalSize { width, height } = self.size;
+
+                let r = x / width as f64;
+                let g = y / height as f64;
+                let b = (x + y) / (height + width) as f64;
+
+                self.color = wgpu::Color { r, g, b, a: 1.0 };
+
+                true
+            }
+            _ => false
+        }
+    }
+
+    pub(crate) fn update(&mut self) {
+    }
+
+    pub(crate) fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+    
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(self.color),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                timestamp_writes: None,
+                occlusion_query_set: None,
+                depth_stencil_attachment: None,
+            });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.draw(0..self.num_vertices, 0..1);
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
---
+++ src/vertex.rs
@@ -0,0 +1,24 @@
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+pub(crate) struct Vertex {
+    pub(crate) position: [f32; 3],
+    pub(crate) color: [f32; 3],
+}
+
+impl Vertex {
+    pub(crate) const ATTRIBBS: [wgpu::VertexAttribute; 2] = wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
+
+    pub(crate) fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBBS,
+        }
+    }
+}
+
+pub(crate) const VERTICES: &[Vertex] = &[
+    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
+    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
+    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+];
